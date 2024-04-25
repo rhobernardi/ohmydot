087bc606f2ac2033ad8bf8caf53bc938e6ec2000
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2024-04-25 14:56:50 -0300

    Fix instalation

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-sudo dnf copr enable skidnik/termite
-sudo dnf update
+sudo dnf copr enable skidnik/termite -y
+sudo dnf update -y
 sudo dnf install -y git vim gcc g++ byobu termite
 
 wget -O ~/.vimrc https://raw.githubusercontent.com/rhobernardi/vimrc/master/vimrc && vim -c 'PlugInstall | q | q'
 
-if [[ ! -d "~/.config" ]]; then
-    mkdir ~.config
-fi
-cp -r config/* ~/.config
-cp bashrc ~/.bashrc
+git clone https://github.com/rhobernardi/ohmydot
+cd ohmydot
 
+if [[ ! -d "~/.config" ]]; then
+    mkdir ~/.config
+fi
+mv config/ ~/.config
+mv bashrc ~/.bashrc
+
+cd
