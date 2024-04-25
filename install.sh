a567e10344a8103490226b5213a7059e64a084ce
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2024-04-25 17:33:18 -0300

    Add alacritty teminal instalation

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-sudo dnf copr enable skidnik/termite -y
+# sudo dnf copr enable skidnik/termite -y
 sudo dnf update -y
-sudo dnf install -y git vim gcc g++ byobu termite
+sudo dnf install -y git vim gcc g++ byobu alacritty
 
 wget -O ~/.vimrc https://raw.githubusercontent.com/rhobernardi/vimrc/master/vimrc && vim -c 'PlugInstall | q | q'
 
 git clone https://github.com/rhobernardi/ohmydot
-cd ohmydot
 
+cd
+mv .bashrc .bashrc.bkp
+
+cd ohmydot
 if [[ ! -d "$HOME/.config" ]]; then
     mkdir $HOME/.config
 fi
 
 cd config
 cp -r $(ls) $HOME/.config
+
 cd ..
 mv bashrc $HOME/.bashrc
-
 cd
