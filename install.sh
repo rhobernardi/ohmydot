c5acd1496019219fafd785995eecf3f7543663bc
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2024-04-29 23:08:06 -0400

    Install ohmyzsh at the end

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ sudo dnf update -y
 sudo dnf install -y git vim gcc g++ byobu zsh polybar alacritty
 
 chsh -s /usr/bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 wget -O ~/.vimrc https://raw.githubusercontent.com/rhobernardi/vimrc/master/vimrc && vim -c 'PlugInstall | q | q'
 
@@ -21,8 +20,11 @@ fi
 cd config
 cp -r $(ls) $HOME/.config
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 cd ..
 mv wallpapers $HOME/Pictures
 mv bashrc $HOME/.bashrc
 mv zshrc $HOME/.zshrc
 cd
+
