6d232dbc885f512fa4508c6bc6550b53eaba3ed5
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2024-04-29 22:05:03 -0400

    Add polybar config and backup

--- backup.sh
+++ backup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 TARGET=$HOME/ohmydot
-if [[ "$1" == "" ]]; then
+if [[ "$1" != "" ]]; then
     TARGET=$1
 fi
 
 CONFIG_PATH=$HOME/.config
 cp -r $CONFIG_PATH/alacritty $TARGET/config
 cp -r $HOME/.config/i3 $TARGET/config
+cp -r $HOME/.config/polybar $TARGET/config
 cp -r $HOME/.config/byobu $TARGET/config
 cp -r $HOME/.config/Thunar $TARGET/config
 cp -r $HOME/.config/xfce4 $TARGET/config
 
-cp $HOME/.zshrc $HOME/.bashrc $HOME/.vimrc $HOME/.fehbg $TARGET
+cp $HOME/.zshrc $TARGET/zshrc
+cp $HOME/.bashrc $TARGET/bashrc
+cp $HOME/.vimrc $TARGET/vimrc
+cp $HOME/.fehbg $TARGET/fehbg
 
 MOZILLA_BKP=$(ls $HOME/.mozilla/firefox | grep -E *.default-release)
 zip -r $MOZILLA_BKP.zip $HOME/.mozilla/firefox/$MOZILLA_BKP
