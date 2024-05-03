df780098bf6da1fd0b23d34b5489ef88b5681400
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2024-05-03 14:10:38 -0300

    Add gedit backup

--- backup.sh
+++ backup.sh
@@ -12,14 +12,14 @@ cp -r $HOME/.config/polybar $TARGET/config
 cp -r $HOME/.config/byobu $TARGET/config
 cp -r $HOME/.config/Thunar $TARGET/config
 cp -r $HOME/.config/xfce4 $TARGET/config
+cp -r $HOME/.config/gedit $TARGET/config
 
 cp $HOME/.zshrc $TARGET/zshrc
 cp $HOME/.bashrc $TARGET/bashrc
 cp $HOME/.aliases $TARGET/aliases
 cp $HOME/.vimrc $TARGET/vimrc
-cp $HOME/.fehbg $TARGET/fehbg
 
-cp -r $HOME/Pictures/wallpapers $TARGET/wallpapers
+cp -r $HOME/Pictures/wallpapers/* $TARGET/wallpapers
 
 git status
 
