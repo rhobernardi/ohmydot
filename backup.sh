e1551fa872549007389b58f53c58a6d21f4dd46c
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2024-05-20 21:49:01 -0300

    Backup picom configs and remove git plugin

--- backup.sh
+++ backup.sh
@@ -9,6 +9,7 @@ CONFIG_PATH=$HOME/.config
 cp -r $CONFIG_PATH/alacritty $TARGET/config
 cp -r $HOME/.config/i3 $TARGET/config
 cp -r $HOME/.config/polybar $TARGET/config
+cp -r $HOME/.config/picom $TARGET/config
 cp -r $HOME/.config/Thunar $TARGET/config
 # cp -r $HOME/.config/byobu $TARGET/config
 # cp -r $HOME/.config/xfce4 $TARGET/config
