17830aa17a641cc4887439da80c4622344dc0ccb
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2024-11-03 20:29:07 -0300

    Change folder name to configs and clean themes

---
+++ EndeavourOS/xfce4/configs/themes/eos/backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TARGET=$HOME/ohmydot/EndeavourOS/xfce4/configs/themes/eos
+if [[ "$1" != "" ]]; then
+    TARGET=$1
+fi
+
+# Copy configs from .config
+CONFIG_PATH=$HOME/.config
+cp -r $CONFIG_PATH/alacritty $TARGET/.config
+cp -r $CONFIG_PATH/btop $TARGET/.config
+cp -r $CONFIG_PATH/gtk-3.0 $TARGET/.config
+cp -r $CONFIG_PATH/gtk-4.0 $TARGET/.config
+cp -r $CONFIG_PATH/Thunar $TARGET/.config
+cp -r $CONFIG_PATH/xfce4 $TARGET/.config
+
+# Copy other folders
+cp -r $HOME/.themes/* $TARGET/.themes
+cp -r $HOME/.icons/* $TARGET/.icons
+cp -r $HOME/.wallpapers/* $TARGET/.wallpapers
+
+# Copy run commands files
+cp $HOME/.gitconfig $TARGET
+cp $HOME/.zshrc $TARGET
+cp $HOME/.bashrc $TARGET
+cp $HOME/.aliases $TARGET
+
+git status
+
--- EndeavourOS/xfce4/install.sh
+++ EndeavourOS/xfce4/install.sh
@@ -10,7 +10,7 @@ export OHMYDOT_PATH
 RUNNING_XFCE=$([[ "$XDG_CURRENT_DESKTOP" == *"XFCE"* ]] && echo true || echo false)
 
 # Check the distribution name and version and abort if incompatible
-source $OHMYDOT_PATH/install/modules/terminal/gum.sh >/dev/null
+source $OHMYDOT_PATH/configs/modules/terminal/gum.sh >/dev/null
 
 if $RUNNING_XFCE; then
     # Ensure computer doesn't go to sleep or lock while installing
@@ -19,20 +19,21 @@ if $RUNNING_XFCE; then
     xset -b
 
     echo "Get ready to chose your theme..."
-    source $OHMYDOT_PATH/install/first-run-choices.sh
+    source $OHMYDOT_PATH/configs/first-run-choices.sh
     echo "Installing terminal and desktop tools..."
 else
     echo "Only installing terminal tools..."
 fi
 
 # Install terminal tools
-source $OHMYDOT_PATH/install/install_modules.sh $OHMYDOT_XFCE_THEME
+source $OHMYDOT_PATH/configs/install_modules.sh $OHMYDOT_XFCE_THEME
 if $RUNNING_XFCE; then
     # Install desktop tools and tweaks
-    source $OHMYDOT_PATH/install/apply_theme.sh $OHMYDOT_XFCE_THEME
+    source $OHMYDOT_PATH/configs/apply_theme.sh $OHMYDOT_XFCE_THEME
 
     # Revert to normal idle and lock settings
     #xset s on
 fi
+
 sleep 1
 reboot
